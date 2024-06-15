936554256fbf8cd7018331e6540fa1397b9b68c2
devholiday <devholidaydev@gmail.com>
2024-06-15 13:02:35 +0300

    remove schema-bricks
     On branch main
     Your branch is ahead of 'origin/main' by 1 commit.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	deleted:    src/controllers/schema-brick/schema-bricks.controller.ts
    	deleted:    src/models/schema-brick.model.ts
    	modified:   src/routes/index.ts
    	deleted:    src/routes/schema-bricks.route.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,6 @@
 import { RoutesInput } from '@/types/types'
 import structure from './structure.route'
-import schemabricks from './schema-bricks.route'
 
 export default ({ app }: RoutesInput) => {
     app.use('/api/structures', structure);
-    app.use('/api/schema_bricks', schemabricks);
 };
